89a1f9bd03ef072f69654c8f4065d7369105dd63
marinaBergas <Mohamed.khaled+1-at-787715290512>
2023-08-30 11:09:55 +0300

    update changenotifier to value instead of create

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -7,11 +7,10 @@ import '../providers/products_providers.dart';
 class ProductGrid extends StatelessWidget {
   const ProductGrid({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    final productData=Provider.of<Products>(context);
-    final productItems=productData.items;
+    final productData = Provider.of<Products>(context);
+    final productItems = productData.items;
     return GridView.builder(
         padding: const EdgeInsets.all(10.0),
         itemCount: productItems.length,
@@ -21,13 +20,10 @@ class ProductGrid extends StatelessWidget {
             crossAxisSpacing: 10,
             mainAxisSpacing: 10),
         itemBuilder: (ctx, index) {
-          return ChangeNotifierProvider(child:const ProductItem(
-              // imageUrl: productItems[index].imageUrl,
-              // id: productItems[index].id,
-              // title: productItems[index].title
-              ),
-              create: (BuildContext context)=> productItems[index],
-               ); 
+          return ChangeNotifierProvider.value(
+            value: productItems[index],
+            child: const ProductItem(),
+          );
         });
   }
 }
