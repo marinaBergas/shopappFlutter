1bd1585e79faf0d2fc454f7252cd04f70957a4d8
marinaBergas <Mohamed.khaled+1-at-787715290512>
2023-08-30 15:12:14 +0300

    add cart provider modal

---
+++ lib/providers/cart.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+  CartItem(
+      {required this.id,
+      required this.title,
+      required this.quantity,
+      required this.price});
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (exisitingCartItem) => CartItem(
+              id: exisitingCartItem.id,
+              title: exisitingCartItem.title,
+              quantity: exisitingCartItem.quantity + 1,
+              price: exisitingCartItem.price));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              quantity: 1,
+              price: price));
+    }
+  }
+}
