2d37ce5bbf3c3f309e3b40239ba7c1b8456d16ba
marina saber <marina.saber.ext@orange.com>
2023-10-10 16:23:18 +0300

    feature: update animation widgets

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -109,18 +109,25 @@ class AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
   late AnimationController _controller;
-  late Animation<Size> _heightAnimation;
+  // late Animation<Size> _heightAnimation;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _opacityAnimation;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 300));
-    _heightAnimation = Tween<Size>(
-            begin: const Size(double.infinity, 260),
-            end: const Size(double.infinity, 320))
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+    // _heightAnimation = Tween<Size>(
+    //         begin: const Size(double.infinity, 260),
+    //         end: const Size(double.infinity, 320))
+    //     .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
     // _heightAnimation.addListener(() => setState(() {}));
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+    _slideAnimation = Tween<Offset>(
+            begin: const Offset(0, -1.5), end: const Offset(0, 0))
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
   }
 
   @override
@@ -218,15 +225,16 @@ class AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-          animation: _heightAnimation,
-          builder: (ctx, ch) => Container(
-              height: _heightAnimation.value.height,
-              constraints:
-                  BoxConstraints(minHeight: _heightAnimation.value.height),
-              width: deviceSize.width * 0.75,
-              padding: const EdgeInsets.all(16.0),
-              child: ch),
+      //AnimatedBuilder AnimatedBuilder (animation: _heightAnimation,builder:(ctx,child)=>
+      child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeIn,
+          height: _authMode == AuthMode.SignUp ? 320 : 260,
+          //_heightAnimation.value.height
+          constraints: BoxConstraints(
+              minHeight: _authMode == AuthMode.SignUp ? 320 : 260),
+          width: deviceSize.width * 0.75,
+          padding: const EdgeInsets.all(16.0),
           child: Form(
             key: _formKey,
             child: SingleChildScrollView(
@@ -259,21 +267,35 @@ class AuthCardState extends State<AuthCard>
                       _authData['password'] = value!;
                     },
                   ),
-                  if (_authMode == AuthMode.SignUp)
-                    TextFormField(
-                      enabled: _authMode == AuthMode.SignUp,
-                      decoration:
-                          const InputDecoration(labelText: 'Confirm Password'),
-                      obscureText: true,
-                      validator: _authMode == AuthMode.SignUp
-                          ? (value) {
-                              if (value != _passwordController.text) {
-                                return 'Passwords do not match!';
-                              }
-                              return null;
-                            }
-                          : null,
+                  // if (_authMode == AuthMode.SignUp)
+                  //when the opacity 0 the space as is it so we need to remove this space when opacity 0 wrap it with container with height
+                  AnimatedContainer(
+                    constraints: BoxConstraints(
+                        minHeight: _authMode == AuthMode.SignUp ? 60 : 0,
+                        maxHeight: _authMode == AuthMode.SignUp ? 120 : 0),
+                    curve: Curves.easeIn,
+                    duration: const Duration(milliseconds: 300),
+                    child: FadeTransition(
+                      opacity: _opacityAnimation,
+                      child: SlideTransition(
+                        position: _slideAnimation,
+                        child: TextFormField(
+                          enabled: _authMode == AuthMode.SignUp,
+                          decoration: const InputDecoration(
+                              labelText: 'Confirm Password'),
+                          obscureText: true,
+                          validator: _authMode == AuthMode.SignUp
+                              ? (value) {
+                                  if (value != _passwordController.text) {
+                                    return 'Passwords do not match!';
+                                  }
+                                  return null;
+                                }
+                              : null,
+                        ),
+                      ),
                     ),
+                  ),
                   const SizedBox(
                     height: 20,
                   ),
